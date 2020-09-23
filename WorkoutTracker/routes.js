8489c1da2cf1095c12ef691775e9d0eef14675b9
Eric Dang <edang830@gmail.com>
2020-09-22 18:06:33 -0700

    Refactor Root component into routes.js

--- WorkoutTracker/routes.js
+++ WorkoutTracker/routes.js
@@ -1,11 +1,16 @@
 import * as React from 'react';
+import {useSelector} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from './src/components/Home';
+import AuthContainer from './src/auth/containers/auth.container';
 import MovementNavigator from './src/movements/movements.navigator';
 
 const tabNavigator = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-export default MainNavigator = () => {
+const MainNavigator = () => {
     return (
         <tabNavigator.Navigator>
             <tabNavigator.Screen name="Home" component={Home} />
@@ -14,3 +19,17 @@ export default MainNavigator = () => {
         </tabNavigator.Navigator>
     );
 };
+
+export default function WorkoutTracker() {
+    const auth = useSelector((state) => state.auth);
+    console.log(auth);
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator headerMode="none">
+                {auth.isAuthenticated && <Stack.Screen name="Main" component={MainNavigator} />}
+                {!auth.isAuthenticated && <Stack.Screen name="Auth" component={AuthContainer} />}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
