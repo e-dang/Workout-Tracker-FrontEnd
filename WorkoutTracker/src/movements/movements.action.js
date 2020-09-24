23ef155523e9e9ee78b74e714dd107bef8e0fc8f
Eric Dang <edang830@gmail.com>
2020-09-24 16:54:36 -0700

    Remove commented out code

--- WorkoutTracker/src/movements/movements.action.js
+++ WorkoutTracker/src/movements/movements.action.js
@@ -4,25 +4,6 @@ import {extractRelatedObjLink} from '../utils/action-helpers';
 import client from '../api/client';
 import {movementListSchema, movementSchema} from '../api/schemas';
 
-// export const createMovement = (name, equipment, muscles) => async (dispatch, getState) => {
-//     const authState = getState().auth;
-
-//     dispatch({type: CREATE_MOVEMENT.PENDING});
-//     return api
-//         .createMovement(authState.user.id, authState.authToken, {
-//             name,
-//             equipment: extractRelatedObjLink(equipment),
-//             muscles: extractRelatedObjLink(muscles),
-//         })
-//         .then((resp) => {
-//             dispatch({type: CREATE_MOVEMENT.SUCCESS, payload: resp.data});
-//         })
-//         .catch((err) => {
-//             dispatch({type: CREATE_MOVEMENT.FAILURE, payload: err.response.status});
-//             throw err;
-//         });
-// };
-
 export const movementActions = {
     listMovements: (userID, paginationParams = {}) => {
         return client.list(`users/${userID}/movements/`, movementListSchema, GET_MOVEMENTS, {
