7399cd9bf62d08b77e0c2ccbc717b6d446260028
Eric Dang <edang830@gmail.com>
2020-09-23 21:58:05 -0700

    Implement pagination functionality

--- WorkoutTracker/src/muscles/containers/muscles-select.container.js
+++ WorkoutTracker/src/muscles/containers/muscles-select.container.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import MusclesSelectScreen from '../screens/muscles-select.screen';
 import {useSelector} from 'react-redux';
-import {addSelectedMuscles} from '../muscles.action';
+import {addSelectedMuscles, muscleActions} from '../muscles.action';
 import {useDispatch} from 'react-redux';
+
+const getMuscles = () => {
+    const paginationState = useSelector((state) => state.pagination);
+    const entityState = useSelector((state) => state.entities);
+
+    try {
+        ids = paginationState.GET_MUSCLES['1'].ids;
+    } catch (TypeError) {
+        ids = [];
+    }
+    return ids.map((id) => entityState.muscles[id]);
+};
 
 export default function MusclesSelectContainer({navigation}) {
     const dispatch = useDispatch();
     const muscleState = useSelector((state) => state.muscles);
+    const paginationState = useSelector((state) => state.pagination);
+    const muscles = getMuscles();
+
+    const refresh = () => dispatch(muscleActions.listMuscles());
+
+    React.useEffect(() => {
+        refresh();
+    });
 
     const handleSelectMuscles = (muscles) => {
         dispatch(addSelectedMuscles(muscles)).then(() => {
@@ -17,9 +37,12 @@ export default function MusclesSelectContainer({navigation}) {
     return (
         <MusclesSelectScreen
             navigation={navigation}
-            muscles={muscleState.muscles}
+            muscles={muscles}
             selectedMuscles={muscleState.selectedMuscles}
             handleSelectMuscles={handleSelectMuscles}
+            onRefresh={refresh}
+            refreshing={!muscles && paginationState.muscles.isFetching}
+            onEndReached={() => dispatch(muscleActions.listMuscles({loadMore: true}))}
         />
     );
 }
