3453e2d50729239dc115a66222f3bf10c72d0ca7
Eric Dang <edang830@gmail.com>
2020-09-24 14:46:50 -0700

    Implement createMovement action

--- WorkoutTracker/src/movements/movements.action.js
+++ WorkoutTracker/src/movements/movements.action.js
@@ -1,28 +1,27 @@
 import {CREATE_MOVEMENT} from './movements.type';
 import {GET_MOVEMENTS} from '../pagination/pagination.action';
-import {api} from '../api';
 import {extractRelatedObjLink} from '../utils/action-helpers';
 import client from '../api/client';
-import {movementListSchema} from '../api/schemas';
+import {movementListSchema, movementSchema} from '../api/schemas';
 
-export const createMovement = (name, equipment, muscles) => async (dispatch, getState) => {
-    const authState = getState().auth;
+// export const createMovement = (name, equipment, muscles) => async (dispatch, getState) => {
+//     const authState = getState().auth;
 
-    dispatch({type: CREATE_MOVEMENT.PENDING});
-    return api
-        .createMovement(authState.user.id, authState.authToken, {
-            name,
-            equipment: extractRelatedObjLink(equipment),
-            muscles: extractRelatedObjLink(muscles),
-        })
-        .then((resp) => {
-            dispatch({type: CREATE_MOVEMENT.SUCCESS, payload: resp.data});
-        })
-        .catch((err) => {
-            dispatch({type: CREATE_MOVEMENT.FAILURE, payload: err.response.status});
-            throw err;
-        });
-};
+//     dispatch({type: CREATE_MOVEMENT.PENDING});
+//     return api
+//         .createMovement(authState.user.id, authState.authToken, {
+//             name,
+//             equipment: extractRelatedObjLink(equipment),
+//             muscles: extractRelatedObjLink(muscles),
+//         })
+//         .then((resp) => {
+//             dispatch({type: CREATE_MOVEMENT.SUCCESS, payload: resp.data});
+//         })
+//         .catch((err) => {
+//             dispatch({type: CREATE_MOVEMENT.FAILURE, payload: err.response.status});
+//             throw err;
+//         });
+// };
 
 export const movementActions = {
     listMovements: (userID, paginationParams = {}) => {
@@ -30,5 +29,13 @@ export const movementActions = {
             ...paginationParams,
             paginationKey: userID.toString(),
         });
+    },
+    createMovement: (userID, {name, equipment, muscles}) => {
+        return client.create(
+            `users/${userID}/movements/`,
+            {name, equipment: extractRelatedObjLink(equipment), muscles: extractRelatedObjLink(muscles)},
+            movementSchema,
+            CREATE_MOVEMENT,
+        );
     },
 };
