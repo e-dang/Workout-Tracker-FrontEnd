695f6b16d0aef31977f738bead278ec3f3b0c9a2
Eric Dang <edang830@gmail.com>
2020-09-23 22:49:41 -0700

    Remove GET_EQUIPMENT from equipment actions

--- WorkoutTracker/src/equipment/equipment.action.js
+++ WorkoutTracker/src/equipment/equipment.action.js
@@ -1,19 +1,7 @@
-import {GET_EQUIPMENT, SELECT_EQUIPMENT, CLEAR_SELECTED_EQUIPMENT} from './equipment.type';
-import {api} from '../api';
-
-export const getEquipment = () => async (dispatch, getState) => {
-    const authState = getState().auth;
-
-    dispatch({type: GET_EQUIPMENT.PENDING});
-    return api
-        .getEquipment(authState.user.id, authState.authToken)
-        .then((resp) => {
-            dispatch({type: GET_EQUIPMENT.SUCCESS, payload: resp.data.results});
-        })
-        .catch((err) => {
-            dispatch({type: GET_EQUIPMENT.FAILURE, payload: err.response.status});
-        });
-};
+import {SELECT_EQUIPMENT, CLEAR_SELECTED_EQUIPMENT} from './equipment.type';
+import {GET_EQUIPMENT} from '../pagination/pagination.action';
+import client from '../api/client';
+import {equipmentListSchema} from '../api/schemas/equipment';
 
 export const addSelectedEquipment = (equipment) => async (dispatch) => {
     return dispatch({type: SELECT_EQUIPMENT, payload: equipment});
@@ -21,4 +9,13 @@ export const addSelectedEquipment = (equipment) => async (dispatch) => {
 
 export const clearSelectedEquipment = () => async (dispatch) => {
     return dispatch({type: CLEAR_SELECTED_EQUIPMENT});
+};
+
+export const equipmentActions = {
+    listEquipment: (userID, paginationParams = {}) => {
+        return client.list(`/users/${userID}/equipment/`, equipmentListSchema, GET_EQUIPMENT, {
+            ...paginationParams,
+            paginationKey: userID.toString(),
+        });
+    },
 };
--- WorkoutTracker/src/equipment/equipment.reducer.js
+++ WorkoutTracker/src/equipment/equipment.reducer.js
@@ -1,34 +1,12 @@
-import {GET_EQUIPMENT, SELECT_EQUIPMENT, CLEAR_SELECTED_EQUIPMENT} from './equipment.type';
+import {SELECT_EQUIPMENT, CLEAR_SELECTED_EQUIPMENT} from './equipment.type';
 
 const initialState = {
-    equipment: [],
     selectedEquipment: [],
-    hasInitialEquipment: false,
-    isPendingGetEquipment: false,
     error: null,
 };
 
 export const equipmentReducer = (state = initialState, action = {}) => {
     switch (action.type) {
-        case GET_EQUIPMENT.PENDING:
-            return {
-                ...state,
-                isPendingGetEquipment: true,
-                error: null,
-            };
-        case GET_EQUIPMENT.SUCCESS:
-            return {
-                ...state,
-                isPendingGetEquipment: false,
-                hasInitialEquipment: true,
-                equipment: action.payload,
-            };
-        case GET_EQUIPMENT.FAILURE:
-            return {
-                ...state,
-                isPendingGetEquipment: false,
-                error: action.payload,
-            };
         case SELECT_EQUIPMENT:
             return {
                 ...state,
