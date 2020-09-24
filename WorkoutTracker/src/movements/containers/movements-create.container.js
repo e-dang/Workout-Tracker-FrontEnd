7e236c8224075b9c805c350698623c0c29027dd2
Eric Dang <edang830@gmail.com>
2020-09-24 16:53:31 -0700

    Implement use of new createMovement function

--- WorkoutTracker/src/movements/containers/movements-create.container.js
+++ WorkoutTracker/src/movements/containers/movements-create.container.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import MovementCreateScreen from '../screens/movements-create.screen';
-import {createMovement, getMovements} from '../movements.action';
-// import {getEquipment} from '../../equipment/equipment.action';
+import {movementActions} from '../movements.action';
 
 export default function MovementCreateContainer({navigation}) {
     const dispatch = useDispatch();
-    const movementState = useSelector((state) => state.movements);
     const equipmentState = useSelector((state) => state.equipment);
     const muscleState = useSelector((state) => state.muscles);
-
-    // React.useEffect(() => {
-    //     if (!equipmentState.hasInitialEquipment && !equipmentState.isPendingGetEquipment) {
-    //         dispatch(getEquipment());
-    //     }
-    // });
+    const authState = useSelector((state) => state.auth);
 
     const handleCreateMovement = (name, equipment, muscles) => {
-        dispatch(createMovement(name, equipment, muscles))
-            .then(() =>
-                dispatch(getMovements())
-                    .then(() => navigation.pop())
-                    .catch((err) => {}),
-            )
-            .catch((err) => {});
+        dispatch(movementActions.createMovement(authState.user.id, {name, equipment, muscles})).then(() => {
+            navigation.navigate('MovementList', {forceRefresh: true});
+        });
     };
 
     return (
--- WorkoutTracker/src/movements/containers/movements-list.container.js
+++ WorkoutTracker/src/movements/containers/movements-list.container.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MovementListScreen from '../screens/movements-list.screen';
 import {useDispatch, useSelector} from 'react-redux';
 import {movementActions} from '../movements.action';
@@ -6,27 +6,27 @@ import {movementActions} from '../movements.action';
 const getMovements = (paginationKey) => {
     const paginationState = useSelector((state) => state.pagination);
     const entityState = useSelector((state) => state.entities);
+    let ids = [];
 
     try {
         ids = paginationState.GET_MOVEMENTS[paginationKey].ids;
     } catch (TypeError) {
         ids = [];
     }
+
     return ids.map((id) => entityState.movements[id]);
 };
 
-export default function MovementListContainer({navigation}) {
+export default function MovementListContainer({route, navigation}) {
     const dispatch = useDispatch();
     const movementState = useSelector((state) => state.movements);
     const authState = useSelector((state) => state.auth);
     const paginationState = useSelector((state) => state.pagination);
     const movements = getMovements(authState.user.id.toString());
 
-    const refresh = () => dispatch(movementActions.listMovements(authState.user.id));
-
     React.useEffect(() => {
-        refresh();
-    });
+        dispatch(movementActions.listMovements(authState.user.id, route.params));
+    }, [route.params]);
 
     return (
         <MovementListScreen
@@ -34,8 +34,8 @@ export default function MovementListContainer({navigation}) {
             isPendingGetMovements={movementState.isPendingGetMovements}
             navigation={navigation}
             refreshing={!movements && paginationState.GET_MOVEMENTS.isFetching}
-            onRefresh={refresh}
-            onEndReached={() => dispatch(movementActions.listMovements(authState.user.id, {loadmore: true}))}
+            onRefresh={() => dispatch(movementActions.listMovements(authState.user.id, {forceRefresh: true}))}
+            onEndReached={() => dispatch(movementActions.listMovements(authState.user.id, {loadMore: true}))}
         />
     );
 }
