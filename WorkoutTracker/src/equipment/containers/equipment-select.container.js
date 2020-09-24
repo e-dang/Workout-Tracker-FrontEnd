9290bba300a34b3eea0a73837c6f9149baa97573
Eric Dang <edang830@gmail.com>
2020-09-23 22:50:35 -0700

    Setup pagination for EquipmentSelect container

--- WorkoutTracker/src/equipment/containers/equipment-select.container.js
+++ WorkoutTracker/src/equipment/containers/equipment-select.container.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import EquipmentSelectScreen from '../screens/equipment-select.screen';
-import {useSelector} from 'react-redux';
-import {addSelectedEquipment} from '../equipment.action';
-import {useDispatch} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {addSelectedEquipment, equipmentActions} from '../equipment.action';
+
+const getEquipment = (paginationKey) => {
+    const paginationState = useSelector((state) => state.pagination);
+    const entityState = useSelector((state) => state.entities);
+
+    try {
+        ids = paginationState.GET_EQUIPMENT[paginationKey].ids;
+    } catch (TypeError) {
+        ids = [];
+    }
+    return ids.map((id) => entityState.equipment[id]);
+};
 
 export default function EquipmentSelectContainer({navigation}) {
     const dispatch = useDispatch();
     const equipmentState = useSelector((state) => state.equipment);
+    const authState = useSelector((state) => state.auth);
+    const paginationState = useSelector((state) => state.pagination);
+    const equipment = getEquipment(authState.user.id.toString());
+
+    const refresh = () => dispatch(equipmentActions.listEquipment(authState.user.id));
+
+    React.useEffect(() => {
+        refresh();
+    });
 
     const handleSelectEquipment = (equipment) => {
         dispatch(addSelectedEquipment(equipment)).then(() => {
@@ -17,9 +37,12 @@ export default function EquipmentSelectContainer({navigation}) {
     return (
         <EquipmentSelectScreen
             navigation={navigation}
-            equipment={equipmentState.equipment}
+            equipment={equipment}
             selectedEquipment={equipmentState.selectedEquipment}
             handleSelectEquipment={handleSelectEquipment}
+            onRefresh={refresh}
+            refreshing={!equipment && paginationState.GET_EQUIPMENT.isFetching}
+            onEndReached={() => dispatch(equipmentActions.listEquipment(authState.user.id, {loadmore: true}))}
         />
     );
 }
--- WorkoutTracker/src/equipment/screens/equipment-select.screen.js
+++ WorkoutTracker/src/equipment/screens/equipment-select.screen.js
@@ -2,7 +2,15 @@ import React from 'react';
 import MultiSelectScreen from '../../components/multi-select-screen.component';
 
 export default function EquipmentSelectScreen(props) {
-    const {navigation, equipment, selectedEquipment, handleSelectEquipment} = props;
+    const {
+        navigation,
+        equipment,
+        selectedEquipment,
+        handleSelectEquipment,
+        onRefresh,
+        refreshing,
+        onEndReached,
+    } = props;
 
     return (
         <MultiSelectScreen
@@ -10,8 +18,11 @@ export default function EquipmentSelectScreen(props) {
             onBackPress={() => navigation.pop()}
             data={equipment}
             preSelected={selectedEquipment}
-            keyExtractor={(item) => item.name}
+            keyExtractor={(item) => item.id.toString()}
             onSubmit={handleSelectEquipment}
+            onRefresh={onRefresh}
+            refreshing={refreshing}
+            onEndReached={onEndReached}
         />
     );
 }
