9b5e4e8381e99c06071e0b7351d4872c27f311e2
Eric Dang <edang830@gmail.com>
2020-09-27 15:10:04 -0700

    Use ListScreen component in MovementListScreen

--- WorkoutTracker/src/movements/containers/movements-list.container.js
+++ WorkoutTracker/src/movements/containers/movements-list.container.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {useDispatch} from 'react-redux';
 import {MovementListScreen} from '@movements/screens'; //'@movements/screens/movements-list.screen';
 import {listMovements} from '@movements';
-import {getMovements, getMovementState, getPaginationState, getAuthUserID} from '@utils';
+import {getMovements, getPaginationState, getAuthUserID} from '@utils';
 
 export function MovementListContainer({route, navigation}) {
     const dispatch = useDispatch();
@@ -16,7 +16,6 @@ export function MovementListContainer({route, navigation}) {
     return (
         <MovementListScreen
             movements={movements}
-            isPendingGetMovements={getMovementState().isPendingGetMovements}
             navigation={navigation}
             refreshing={!movements && getPaginationState('GET_MOVEMENTS').isFetching}
             onRefresh={() => dispatch(listMovements(userID, {forceRefresh: true}))}
