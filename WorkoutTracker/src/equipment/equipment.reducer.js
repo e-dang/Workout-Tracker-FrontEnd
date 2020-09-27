ea082a2380c08f301541d3b4be05719d7ec70640
Eric Dang <edang830@gmail.com>
2020-09-27 16:13:23 -0700

    Implement createEquipment action

--- WorkoutTracker/src/equipment/equipment.reducer.js
+++ WorkoutTracker/src/equipment/equipment.reducer.js
@@ -1,12 +1,34 @@
-import {SELECT_EQUIPMENT, CLEAR_SELECTED_EQUIPMENT, REMOVE_SELECTED_EQUIPMENT} from '@equipment/equipment.type';
+import {
+    CREATE_EQUIPMENT,
+    SELECT_EQUIPMENT,
+    CLEAR_SELECTED_EQUIPMENT,
+    REMOVE_SELECTED_EQUIPMENT,
+} from '@equipment/equipment.type';
 
 const initialState = {
     selectedEquipment: [],
+    isPendingCreateEquipment: false,
     error: null,
 };
 
 export const equipmentReducer = (state = initialState, action = {}) => {
     switch (action.type) {
+        case CREATE_EQUIPMENT.PENDING:
+            return {
+                ...state,
+                isPendingCreateEquipment: true,
+            };
+        case CREATE_EQUIPMENT.SUCCESS:
+            return {
+                ...state,
+                isPendingCreateEquipment: false,
+            };
+        case CREATE_EQUIPMENT.ERROR:
+            return {
+                ...state,
+                isPendingCreateEquipment: false,
+                error: action.payload,
+            };
         case SELECT_EQUIPMENT:
             return {
                 ...state,
