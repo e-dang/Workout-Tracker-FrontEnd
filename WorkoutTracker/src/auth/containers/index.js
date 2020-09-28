ef9e3862a290c5831fcce24f63bfb627f4a6a738
Eric Dang <edang830@gmail.com>
2020-09-27 17:49:46 -0700

    Change AuthContainer to AuthNavigator

--- WorkoutTracker/src/auth/containers/index.js
+++ WorkoutTracker/src/auth/containers/index.js
@@ -1,4 +1,4 @@
-export * from '@auth/containers/auth.container';
 export * from '@auth/containers/login.container';
 export * from '@auth/containers/signup.container';
 export * from '@auth/containers/welcome.container';
+export * from '@auth/containers/auth.navigator';
