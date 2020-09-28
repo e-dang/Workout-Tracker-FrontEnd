5d2db9b3ca903f83e261f3761c0bab03b01a458d
Eric Dang <edang830@gmail.com>
2020-09-27 17:45:59 -0700

    Remove styling from CustomTextInput component

--- WorkoutTracker/src/components/custom-text-input.component.js
+++ WorkoutTracker/src/components/custom-text-input.component.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View} from 'react-native';
 import {HelperText, TextInput, withTheme} from 'react-native-paper';
 
 function _CustomTextInput(props) {
-    const {style, label, value, onChangeText, errVisible, errMessage} = props;
+    const {style, errVisible, errMessage} = props;
 
     return (
-        <View style={styles.container}>
-            <TextInput style={style} mode="outlined" label={label} value={value} onChangeText={onChangeText} />
+        <View>
+            <TextInput style={style} mode="outlined" {...props} />
             <HelperText type="error" visible={errVisible}>
                 {errMessage}
             </HelperText>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-});
 
 export const CustomTextInput = withTheme(_CustomTextInput);
