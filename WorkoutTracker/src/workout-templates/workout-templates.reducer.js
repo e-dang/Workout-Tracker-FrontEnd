a6f073466d78d7283baf32dc04e6be71727b58d9
Eric Dang <edang830@gmail.com>
2020-09-29 18:52:03 -0700

    Implement COMMIT_WORKOUT_TEMPLATE action

--- WorkoutTracker/src/workout-templates/workout-templates.reducer.js
+++ WorkoutTracker/src/workout-templates/workout-templates.reducer.js
@@ -1,7 +1,17 @@
-import {CREATE_WORKOUT_TEMPLATE} from '@workout-templates/workout-templates.type';
+import {
+    CREATE_WORKOUT_TEMPLATE,
+    UPDATE_WORKOUT_TEMPLATE,
+    COMMIT_WORKOUT_TEMPLATE,
+} from '@workout-templates/workout-templates.type';
+
+import {denormalize} from 'normalizr';
+import {workoutTemplateSchema} from '@api';
 
 const initalState = {
+    uncommittedWorkoutTemplate: null,
+    isBuildingWorkoutTemplate: false,
     isPendingCreateWorkoutTemplate: false,
+    isPendingUpdateWorkoutTemplate: false,
     error: null,
 };
 
@@ -11,17 +21,50 @@ export const workoutTemplateReducer = (state = initalState, action) => {
             return {
                 ...state,
                 isPendingCreateWorkoutTemplate: true,
+                isBuildingWorkoutTemplate: false,
+                uncommittedWorkoutTemplate: null,
+                error: null,
             };
         case CREATE_WORKOUT_TEMPLATE.SUCCESS:
             return {
                 ...state,
                 isPendingCreateWorkoutTemplate: false,
+                isBuildingWorkoutTemplate: true,
+                uncommittedWorkoutTemplate: denormalize(
+                    action.payload.result,
+                    workoutTemplateSchema,
+                    action.payload.entities,
+                ),
             };
         case CREATE_WORKOUT_TEMPLATE.ERROR:
             return {
                 ...state,
                 isPendingCreateWorkoutTemplate: false,
                 error: action.payload,
+            };
+        case UPDATE_WORKOUT_TEMPLATE.PENDING:
+            return {
+                ...state,
+                isPendingUpdateWorkoutTemplate: true,
+                error: null,
+            };
+        case UPDATE_WORKOUT_TEMPLATE.SUCCESS:
+            return {
+                ...state,
+                isPendingUpdateWorkoutTemplate: false,
+                uncommittedWorkoutTemplate: action.payload,
+            };
+        case UPDATE_WORKOUT_TEMPLATE.ERROR:
+            return {
+                ...state,
+                isPendingUpdateWorkoutTemplate: false,
+                error: action.payload,
+            };
+        case COMMIT_WORKOUT_TEMPLATE:
+            return {
+                uncommittedWorkoutTemplate: null,
+                isBuildingWorkoutTemplate: false,
+                error: null,
             };
         default:
             return state;
