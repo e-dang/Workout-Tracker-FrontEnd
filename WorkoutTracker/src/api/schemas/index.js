ae03ad9a4e5abad843311c6cdf038006b35bd4a6
Eric Dang <edang830@gmail.com>
2020-09-29 18:50:27 -0700

    Add export of exercise-template schemas

--- WorkoutTracker/src/api/schemas/index.js
+++ WorkoutTracker/src/api/schemas/index.js
@@ -3,3 +3,4 @@ export * from '@api/schemas/users';
 export * from '@api/schemas/movements';
 export * from '@api/schemas/muscles';
 export * from '@api/schemas/workout-templates';
+export * from '@api/schemas/exercise-templates';
