cd04a03251f5d6a3ebe280ae6b55e0ea1115ceb3
Eric Dang <edang830@gmail.com>
2020-10-01 13:32:57 -0700

    Implement WorkoutTemplateCreate container and screen

--- WorkoutTracker/src/workout-templates/containers/workout-template-create.container.js
+++ WorkoutTracker/src/workout-templates/containers/workout-template-create.container.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import {useDispatch} from 'react-redux';
+import {
+    deleteWorkoutTemplate,
+    commitWorkoutTemplate,
+    refreshWorkoutTemplate,
+    createWorkoutTemplate,
+} from '@workout-templates';
 import {WorkoutTemplateCreateScreen} from '@workout-templates/screens';
+import {getUncommittedWorkoutTemplate, getAuthUserID} from '@utils';
 
-export function WorkoutTemplateCreateContainer({navigation}) {
+export function WorkoutTemplateCreateContainer({route, navigation}) {
     const dispatch = useDispatch();
+    const userID = getAuthUserID();
+    const workoutTemplate = getUncommittedWorkoutTemplate();
 
-    return <WorkoutTemplateCreateScreen navigation={navigation} exerciseTemplates={[]} />;
+    React.useEffect(() => {
+        if (workoutTemplate == null) {
+            dispatch(createWorkoutTemplate(userID, {exercises: []}));
+        }
+    }, []);
+
+    React.useEffect(() => {
+        if (route.params && route.params.forceRefresh) {
+            dispatch(refreshWorkoutTemplate(workoutTemplate));
+        }
+    }, [route.params]);
+
+    const onSubmit = () => {
+        dispatch(commitWorkoutTemplate()).then(() => navigation.pop());
+    };
+
+    const onBack = () => {
+        dispatch(deleteWorkoutTemplate(workoutTemplate)).then(() =>
+            dispatch(commitWorkoutTemplate()).then(() =>
+                navigation.navigate('ListWorkoutTemplate', {forceRefresh: true}),
+            ),
+        );
+    };
+
+    return (
+        <WorkoutTemplateCreateScreen
+            navigation={navigation}
+            workoutTemplate={workoutTemplate || undefined}
+            onBack={onBack}
+            onSubmit={onSubmit}
+        />
+    );
 }
--- WorkoutTracker/src/workout-templates/screens/workout-template-create.screen.js
+++ WorkoutTracker/src/workout-templates/screens/workout-template-create.screen.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import {Appbar} from 'react-native-paper';
-import {ContainerView, AddList} from '@components';
+import {ContainerView, AddList, ShortTextInput, SubmitButton} from '@components';
+
+const getExerciseString = (exercise) => {
+    let str = `${exercise.name} `;
+    let workload;
+    for (workload of exercise.workloads) {
+        str = str.concat(`${workload.sets.length} sets`);
+    }
+    return str;
+};
 
 export function WorkoutTemplateCreateScreen(props) {
-    const {navigation, exerciseTemplates} = props;
+    const {navigation, workoutTemplate = {exercises: []}, onSubmit, onBack} = props;
 
     return (
         <ContainerView>
             <Appbar.Header>
-                <Appbar.BackAction onPress={() => navigation.pop()} />
+                <Appbar.BackAction onPress={onBack} />
                 <Appbar.Content title="Create Workout" />
             </Appbar.Header>
-            <AddList data={exerciseTemplates} onAdd={() => navigation.push('CreateExercise')} />
+            <ShortTextInput label="Name" />
+            <AddList
+                data={workoutTemplate.exercises}
+                titleExtractor={getExerciseString}
+                keyExtractor={(item, idx) => `${idx}`}
+                onAdd={() => navigation.push('SelectMovement')}
+            />
+            <SubmitButton onPress={onSubmit} />
         </ContainerView>
     );
 }
